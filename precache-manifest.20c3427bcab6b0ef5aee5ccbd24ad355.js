06f3145d842d69904e96565a11e6b3a6e073e252
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-29 18:45:14 +0000

    Deploy Superjo149/bondingCurvesUX to github.com/Superjo149/bondingCurvesUX.git:gh-pages

---
+++ precache-manifest.20c3427bcab6b0ef5aee5ccbd24ad355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea4fd8ff862e9c246f85",
+    "url": "/bondingCurvesUX/static/css/main.3e4bc98b.chunk.css"
+  },
+  {
+    "revision": "ea4fd8ff862e9c246f85",
+    "url": "/bondingCurvesUX/static/js/main.ea4fd8ff.chunk.js"
+  },
+  {
+    "revision": "79062a05bc35f09bf0ce",
+    "url": "/bondingCurvesUX/static/css/1.65bed076.chunk.css"
+  },
+  {
+    "revision": "79062a05bc35f09bf0ce",
+    "url": "/bondingCurvesUX/static/js/1.79062a05.chunk.js"
+  },
+  {
+    "revision": "05e376552353e679b39a",
+    "url": "/bondingCurvesUX/static/js/runtime~main.05e37655.js"
+  },
+  {
+    "revision": "ae84296f3b9ccb7054530d3af623f1fa",
+    "url": "/bondingCurvesUX/static/media/logo.ae84296f.svg"
+  },
+  {
+    "revision": "7687ee75e02fb094189d5c2c0d4576e3",
+    "url": "/bondingCurvesUX/index.html"
+  }
+];
